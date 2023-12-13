2808e5f035ef5bd4f702ba40a2d0632b5d15f2db
cvamshi0204 <149609985+cvamshi0204@users.noreply.github.com>
2023-12-13 21:16:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,42 +3,46 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost/registration-form', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost/user-registration', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
-// Create a user schema
 const userSchema = new mongoose.Schema({
-  username: String,
   email: String,
   password: String
 });
 
 const User = mongoose.model('User', userSchema);
 
-// Middleware to parse JSON requests
-app.use(bodyParser.json());
-
-// Set up static files (CSS)
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// Set up HTML form
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-// Handle form submissions
+app.get('/signin', (req, res) => {
+  res.sendFile(__dirname + '/views/signin.html');
+});
+
 app.post('/register', async (req, res) => {
   try {
-    const { username, email, password } = req.body;
-    const newUser = new User({ username, email, password });
+    const { email, password } = req.body;
+    const newUser = new User({ email, password });
     await newUser.save();
     res.send('Registration successful!');
   } catch (error) {
     res.status(500).send('Error during registration.');
   }
 });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
