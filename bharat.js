7c8d9bdef7d430a8b0c94ad67cf513a4d42f03fb
cvamshi0204 <149609985+cvamshi0204@users.noreply.github.com>
2023-12-13 21:30:26 +0530

    Update bharat.js

--- bharat.js
+++ bharat.js
@@ -1,17 +1,1 @@
-// Get the form element by its id
-var form = document.getElementById("form");
 
-// Add an event listener to the form when it is submitted
-form.addEventListener("submit", function(event) {
-    // Get the username and password input values by their ids
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-
-    // Check if the username or password is empty
-    if (username == "" || password == "") {
-        // Prevent the form from submitting
-        event.preventDefault();
-        // Display an alert message
-        alert("Please enter your username and password");
-    }
-});
